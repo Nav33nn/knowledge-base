95c3f032479d03ac3070054b2ecd1730dafd83c8
nav33nn <naveen.bharadwaj@pramata.com>
2018-10-25 22:50:45 +0530

    script fix

--- nlu_chatbot/foodie-bot-master/env_setup.sh
+++ nlu_chatbot/foodie-bot-master/env_setup.sh
@@ -3,4 +3,4 @@ pip install -r requirements.txt
 python -m spacy download en
 pip install rasa-core==0.11.12
 pip install rasa-core-sdk==0.11.5
-pip install rasa-nlu=0.13.7
+pip install rasa-nlu==0.13.7
