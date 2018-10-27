b7d668f99dac508fad22b8613228dfeb36732229
nav33nn <naveen.bharadwaj@pramata.com>
2018-10-27 10:54:47 +0530

    email and known cities commit

--- nlu_chatbot/foodie-bot-master/actions.py
+++ nlu_chatbot/foodie-bot-master/actions.py
@@ -22,9 +22,9 @@ class ActionSearchRestaurants(Action):
         d1 = json.loads(location_detail)
         lat = d1["location_suggestions"][0]["latitude"]
         lon = d1["location_suggestions"][0]["longitude"]
-        city_name = d1["location_suggestions"][0]["city_name"]
+        city_id = d1["location_suggestions"][0]["city_id"]
         known_city_ids = pickle.load('data/known_cities_id.pkl','rb')
-        if city_name.lower() in known_cities.lower():            
+        if city_id in known_city_ids:            
             cuisines_dict = {'bakery': 5, 'chinese': 25, 'cafe': 30, 'italian': 55, 'biryani': 7, 'north indian': 50,
                              'south indian': 85}
             results = zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
@@ -55,9 +55,9 @@ class ActionSuggest(Action):
                                  "find anything else :)")
         return [SlotSet('matches')]
 
-class ActionAskEmailID(Action):
+class ActionSendEmail(Action):
     def name(self):
-        return 'utter_ask_email_id'
+        return 'utter_send_email'
 
     def run(self, dispatcher, tracker, domain):
         email_id = tracker.get_slot('email')
@@ -79,7 +79,24 @@ class ActionAskEmailID(Action):
         city_id = get_city_ID(loc)
         location_detail = zomato.get_location(loc, 1)
         d1 = json.loads(location_detail)
-        body = 
+        city_id = d1["location_suggestions"][0]["city_id"]
+        known_city_ids = pickle.load('data/known_cities_id.pkl','rb')
+        if city_id in known_city_ids:            
+            cuisines_dict = {'bakery': 5, 'chinese': 25, 'cafe': 30, 'italian': 55, 'biryani': 7, 'north indian': 50,
+                             'south indian': 85}
+            results = zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
+            d = json.loads(results)
+            response = ""
+            if d['results_found'] == 0:
+                response = "no results"
+            else:
+                for restaurant in d['restaurants']:
+                    response = response + "Found " + restaurant['restaurant']['name'] + " in " \
+                               + restaurant['restaurant']['location']['address'] + "\n"
+        else:
+            body = 'The city you had selected do not belong to Tier 1 or Tier 2 cities. Apologies extended.'
+
+        body = response
         msg = 'Subject: {}\n\n{}'.format(mail_subject, body)
 
         server.sendmail("bot.foodie.01@gmail.com", email_id, msg)
